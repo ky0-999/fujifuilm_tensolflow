698a2d6ba5c54a1476e5fb1e060e0cc6e0b48e16
kyo_99:) <124654406+ky0-999@users.noreply.github.com>
2023-08-29 00:20:00 +0900

    Update tensorflow2pytorch.py

--- tensorflow2pytorch.py
+++ tensorflow2pytorch.py
@@ -1,49 +1,21 @@
 import tensorflow as tf
-from tensorflow.keras.applications import EfficientNetV2B0
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
-
-input_shape = (256, 256, 6)  # 入力画像サイズとチャンネル数
-output_units = 1  # 出力層のユニット数
-
-# 入力層を定義
-input_layer = Input(shape=input_shape)
-
-# EfficientNetV2モデルを読み込む
-effnet_model = EfficientNetV2B0(input_tensor=input_layer, include_top=False, weights='imagenet')
-
-# プーリング層などを含むEfficientNetV2の出力をフラットにする
-flatten_layer = tf.keras.layers.Flatten()(effnet_model.output)
-
-# 回帰のための出力層を定義
-output_layer = Dense(output_units, activation='linear')(flatten_layer)
-
-# モデルを結合
-model = Model(inputs=input_layer, outputs=output_layer)
-
-# モデルをコンパイル
-model.compile(optimizer='adam', loss='mean_squared_error')
-
-# モデルのサマリを表示
-model.summary()
-
-
-
-import tensorflow as tf
-from tensorflow.keras.layers import Input
 from tensorflow.keras.applications import EfficientNetV2
-
 
 # 入力データの準備
 input_shape = (256, 256, 6)
 input_tensor = Input(shape=input_shape)
 
 # EfficientNetV2モデルの構築
-model = tf.keras.Sequential()
-model.add(input_tensor)  # 入力層の追加
+base_model = EfficientNetV2.EfficientNetV2B0(include_top=False, weights='imagenet', input_tensor=input_tensor)
 
-# EfficientNetV2 v0モデルの追加
-model.add(EfficientNetV2.EfficientNetV2B0(include_top=True, weights=None, input_tensor=input_tensor, classes=1))
+# 新しい出力層の追加
+x = tf.keras.layers.GlobalAveragePooling2D()(base_model.output)
+output_layer = Dense(1, activation='tanh')(x)  # tanh活性化関数を使用
+
+# 新しいモデルの構築
+model = Model(inputs=input_tensor, outputs=output_layer)
 
 # モデルのコンパイル
 model.compile(
@@ -54,4 +26,3 @@ model.compile(
 
 # モデルのサマリーを表示
 model.summary()
-
