ba04b8a15210b40e312e5d648bb99d71943ced94
kyo_99:) <124654406+ky0-999@users.noreply.github.com>
2023-08-29 00:31:40 +0900

    Update data_augumentation.py

--- data_augumentation.py
+++ data_augumentation.py
@@ -21,26 +21,37 @@ def apply_gamma_correction(image, gamma):
     gamma_corrected = ((image / 255.0) ** gamma) * 255.0
     return np.clip(gamma_corrected, 0, 255).astype(np.uint8)
 
-# 画像Aを回転して背景Bに張り付ける
-rotation_angle = 45  # 回転角度（例として45度とします）
-rotated_image_A = rotate_image(image_A, rotation_angle)
+# 画像Aに緑の線を引く関数
+def draw(image):
+    center_x = image.shape[1] // 2
+    cv2.line(image, (center_x, 0), (center_x, image.shape[0]), (0, 255, 0), 2)
 
-# 10枚の合成画像を生成
-num_images = 10
-for i in range(num_images):
-    gamma = random.uniform(0.1, 1.0)  # ランダムなγ値を選択
+# データ拡張を行う関数
+def sample(image):
+    rotated_image_A = rotate_image(image, rotation_angle)
+    gamma = random.uniform(0.1, 1.0)
     gamma_corrected_image_A = apply_gamma_correction(rotated_image_A, gamma)
     
     x_offset = random.randint(0, background_size[0] - gamma_corrected_image_A.shape[1])
     y_offset = random.randint(0, background_size[1] - gamma_corrected_image_A.shape[0])
     
-    # 中心から8方向にランダムに平行移動する
-    directions = [(0, 0), (1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)]
     direction = random.choice(directions)
     new_x_offset = x_offset + direction[0] * 50
     new_y_offset = y_offset + direction[1] * 50
     
     background_B[new_y_offset:new_y_offset+gamma_corrected_image_A.shape[0], new_x_offset:new_x_offset+gamma_corrected_image_A.shape[1]] = gamma_corrected_image_A
+    
+    draw(background_B)  # 画像Aに線を引く
+
+# 画像Aの中心から8方向に平行移動するための方向リスト
+directions = [(0, 0), (1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)]
+
+# 10枚の合成画像を生成
+num_images = 10
+rotation_angle = 45  # 回転角度（例として45度とします）
+
+for i in range(num_images):
+    sample(image_A)
 
 # 合成された画像の表示
 cv2.imshow('Composite Images', background_B)
